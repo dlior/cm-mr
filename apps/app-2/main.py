f95fad8fa72b1c3410db9f921707740f4c638a41
Lior Degu <lior.degu@gmail.com>
2025-06-21 20:52:31 +0300

    fix(app-1,app-2,app-3): chuck norris joke

--- apps/app-2/main.py
+++ apps/app-2/main.py
@@ -3,8 +3,8 @@ import requests
 
 def main():
     print("App 2: Main function executed!")
-    joke = requests.get("https://api.chucknorris.io/jokes/random").json()
-    print(f"joke: {joke['value']}")
+    response = requests.get("https://api.chucknorris.io/jokes/random").json()
+    print(f"joke: {response['value']}")
 
 
 if __name__ == "__main__":
--- apps/app-3/main.go
+++ apps/app-3/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("App 3: Main function executed!")
+
+	response, err := http.Get("https://api.chucknorris.io/jokes/random")
+	if err != nil {
+		log.Fatalf("Error making GET request: %v", err)
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatalf("Error reading response: %v", err)
+	}
+
+	var result struct {
+		Joke string `json:"value"`
+	}
+
+	if err := json.Unmarshal(body, &result); err != nil {
+		log.Fatalf("Error reading response: %v", err)
+	}
+
+	fmt.Println("joke:", string(result.Joke))
+
 }
