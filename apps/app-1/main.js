a2f9a473a0a5251218d22d77d61414873d8516ed
Lior Degu <lior.degu@gmail.com>
2025-06-21 20:55:52 +0300

    fix(app-1): scripts

--- apps/app-1/main.js
+++ apps/app-1/main.js
@@ -1,5 +1,5 @@
 console.log('App 1: Main script executed!');
 fetch('https://api.chucknorris.io/jokes/random')
   .then((response) => response.json())
-  .then(({ joke }) => console.log('joke:', joke))
+  .then(({ value }) => console.log('joke:', value))
   .catch(console.error);
