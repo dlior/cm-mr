0758f3f4ea08129b781053360e6acb59ac674ffb
Lior Degu <lior.degu@gmail.com>
2025-06-21 19:30:04 +0300

    refactor(app-1): log

--- apps/app-1/main.js
+++ apps/app-1/main.js
@@ -1,4 +1,4 @@
-console.log('App 1: Main script executed!');
+console.log('App 1: Main script executed');
 fetch('https://api.chucknorris.io/jokes/random')
   .then((response) => response.json())
   .then(({ value }) => console.log('joke:', value))
