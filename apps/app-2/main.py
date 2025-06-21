cf217832f938d01458426357ce7b199e7333bdb1
Lior Degu <lior.degu@gmail.com>
2025-06-21 19:38:26 +0300

    feat(app-2): log

--- apps/app-2/main.py
+++ apps/app-2/main.py
@@ -2,7 +2,7 @@ import requests
 
 
 def main():
-    print("App 2: Main function executed!")
+    print("App 2: Main function executed")
     joke = requests.get("https://api.chucknorris.io/jokes/random").json()
     print(f"joke: {joke['value']}")
 
